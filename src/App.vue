ffbdc5ce9a38b58e2588c56ef1e92dbbf2e4e857
Eduard Deisling <edikdeisling@gmail.com>
2022-04-26 21:32:35 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+  import { ref } from 'vue';
+
+  const model = ref(new Set());
+</script>
+
+<template>
+  <label>
+    <input type="checkbox" value="1" v-model="model">
+    <span>Checkbox 1</span>
+  </label>
+  <label>
+    <input type="checkbox" value="2" v-model="model">
+    <span>Checkbox 2</span>
+  </label>
+  <div>
+    <br>
+    Current value: {{ model }}
+  </div>
+</template>
+
+<style></style>
